1b410dcd90663157a5f7ed86ce605b8ff2a47773
Pavel Mazaev <pavluntiy96@gmail.com>
2014-04-20 11:04:25 +0400

    Added like buttons (nya!)

--- index.php
+++ index.php
@@ -9,8 +9,27 @@
 		</title>
 		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 		<link type="text/css" rel="stylesheet" href="reset.css"/>
+
+			<!-- Put this script tag to the <head> of your page -->
+		<script type="text/javascript" src="//vk.com/js/api/openapi.js?111"></script>
+
+		<script type="text/javascript">
+		  VK.init({apiId: 4317614, onlyWidgets: true});
+		</script>
 	</head>
 	<body>
+
+		
+		<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/ru_RU/all.js#xfbml=1";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
+
+
 		<div id = "top">
 			<div id = "logo">
 				<?php if($currentPageID != "index"){?> <a href = "index.php"><?php } ?>
@@ -67,11 +86,30 @@
 		</div>
 
 		<div id = "bottom">
-				<div  class = "otherText" id  = "bottomContent">
+			<div  class = "otherText" id  = "bottomContent">
 				<div> If you have problems, <a href = "mailto:master@yapay.esy.es">email</a> us!
 					<i><a href = "mailto:master@yapay.esy.es">master@yapay.esy.es</a></i>
 				</div>
-			</div>
+
+					<div class = "shareList">
+						<ul>
+							<li>
+								<div>
+									<!-- Put this div tag to the place, where the Like block will be -->
+									<div id="vk_like"></div>
+									<script type="text/javascript">
+									VK.Widgets.Like("vk_like", {type: "button"});
+									</script>
+
+								</div>
+								</VK.Widgets.Like("vk_like",>
+							</li>
+							<li>
+								<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true">
+								</div>
+							</li>
+						</ul>
+					</div>
 		</div>
 
 	</body>
